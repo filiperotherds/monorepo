f23c57f9ed891a530470d2821537f31f53eeb4e0
Filipe <filiperotherds@gmail.com>
2025-07-29 13:03:57 -0300

    Otimizando a tipagem das permissões

--- packages/auth/src/subjects/organization.ts
+++ packages/auth/src/subjects/organization.ts
@@ -1,14 +1,14 @@
 import z from 'zod'
 
-export const projectSubject = z.tuple([
+export const organizationSubject = z.tuple([
   z.union([
     z.literal('manage'),
-    z.literal('get'),
     z.literal('create'),
     z.literal('update'),
     z.literal('delete'),
+    z.literal('transfer_ownership'),
   ]),
   z.literal('Project'),
 ])
 
-export type ProjectSubject = z.infer<typeof projectSubject>
+export type OrganizationSubject = z.infer<typeof organizationSubject>
--- packages/auth/src/subjects/user.ts
+++ packages/auth/src/subjects/user.ts
@@ -1,1 +1,14 @@
-export type UserSubject = ['create' | 'delete' | 'invite' | 'manage', 'User']
+import z from 'zod'
+
+export const userSubject = z.tuple([
+  z.union([
+    z.literal('manage'),
+    z.literal('get'),
+    z.literal('create'),
+    z.literal('update'),
+    z.literal('delete'),
+  ]),
+  z.literal('User'),
+])
+
+export type UserSubject = z.infer<typeof userSubject>
